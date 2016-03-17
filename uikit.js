5392f386809d0facf3268d4a108ced5e30b330ec
Carlos Navas <carlosnavas1991@gmail.com>
2016-03-17 12:54:25 +0100

    Updated ui

--- uikit.js
+++ uikit.js
@@ -1,24 +1,5 @@
 $( document ).ready(function() {
 
-  /*var uiMenu = $('.ui-context-menu.example');
-  var zIndex = 1;
-  $('.ui-view').on('contextmenu', function(e){
-
-    zIndex++;
-    console.log(e.pageX+ ' , ' + e.pageY);
-    e.preventDefault();
-    var width   = e.pageX;
-    var heigth  = e.pageY;
-    console.log(uiMenu);
-    uiMenu.clone().css({
-      "zIndex" : zIndex,
-      "position": "absolute",
-      "left": e.pageX,
-      "top": e.pageY
-    }).addClass('created').insertAfter($('.ui-view'));
-
-  });*/
-
   $('.ui-checkbox').on('click', function(){
 
     var object = $(this);
